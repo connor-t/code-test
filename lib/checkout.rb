39bc88673f4bf4fc341a10596e40887afaac36ad
Connor Tinney <connor.tinney@gmail.com>
2020-11-13 09:59:08 +0000

    Checkout is functional, add promotions next

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,9 +1,10 @@
 class Checkout
   attr_accessor :basket
-  
+
   def initialize(promotional_rules)
     @promotional_rules = promotional_rules
     @basket = []
+    @total = 0
   end
 
   def scan(item)
@@ -11,5 +12,6 @@ class Checkout
   end
 
   def total
+    @basket.sum(&:price)
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,19 +3,31 @@ require_relative '../lib/checkout.rb'
 describe 'Checkout' do
   let(:promotional_rules) { 'rules' }
   let(:co) { Checkout.new(promotional_rules) }
-  let(:item) { Product.new(001, 'chair', 9.99) }
+  let(:item) { Product.new(001, 'chair', 9.25) }
+  let(:item_2) { Product.new(002, 'table', 45.0) }
 
   context '#scan' do
     it 'scans an item' do
       co.scan(item)
       expect(co.basket.first).to eq(item)
     end
+
+    it 'can scan an multiple items' do
+      co.scan(item)
+      co.scan(item_2)
+      expect(co.basket.count).to eq(2)
+    end
   end
 
   context '#total' do
-    it 'displays the checkout total' do
+    it 'returns 0 when basket is empty' do
+      expect(co.total).to eq(0)
+    end
+
+    it 'displays the checkout total when the basket has items' do
       co.scan(item)
-      expect(co.total).to eq item.price
+      co.scan(item_2)
+      expect(co.total).to eq(54.25)
     end
   end
 end
